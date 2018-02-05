998c1f6e3fc864ba6fa88e25847d16c6c81f39ca
Matthew Xu <mxu@counterpath.com>
2018-02-05 14:17:52 -0800

    Added '/wizard' - wizard page

---
+++ pages/wizard.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container>
+    <form-wizard>
+      <tab-content title="Upload Images" :before-change="onGetWeidianAttrList">
+        <app-step-one></app-step-one>
+      </tab-content>
+      <tab-content title="SKU">
+
+      </tab-content>
+    </form-wizard>
+  </b-container>
+</template>
+
+<script>
+import StepOneComponent from '@/components/wizard/StepOne'
+
+export default {
+  name: 'wizard',
+  components: {
+    'app-step-one': StepOneComponent
+  },
+  methods: {
+    async onGetWeidianAttrList () {
+      await this.$store.dispatch('WEIDIAN_GET_ATTRIBUTE')
+      return true
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
